f3a751fdfb6d6d02d4abf97423b096890781c326
Geri <78084908+gergelyhornyak@users.noreply.github.com>
2025-12-19 12:16:26 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function updateMessage() {
-    document.getElementById('message').innerText = "The deployment from GitHub worked!";
-    console.log("Button clicked!");
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('message').innerText = "Ready to deploy.";
-});
